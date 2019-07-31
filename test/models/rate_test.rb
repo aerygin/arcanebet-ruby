3840c7ab8ff29dab8bdebe5c4fb239c471ae90ff
artjomj <artjom.jerogin@icefire.ee>
2019-07-31 10:45:19 +0300

    uncommented "test" tests

--- test/models/rate_test.rb
+++ test/models/rate_test.rb
@@ -3,15 +3,13 @@
 require 'test_helper'
 
 class RateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  #
+   test "the truth" do
+     assert true
+   end
 
-=begin
+
   test "week number validation should trigger " do
     assert_not Rate.new(amount: 100, user_id: 5,  weeks: "123").save
   end
-=end
 
 end
