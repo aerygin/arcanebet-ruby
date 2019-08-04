9bbd75423c5b627bf4fb94f0176c1b3b18ef9b40
artjomj <artjom.jerogin@icefire.ee>
2019-08-04 22:55:33 +0300

    fixer service test

---
+++ test/services/fixer_result_service_test.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class FixerResultServiceTest < ActiveSupport::TestCase
+  RATE = Rate.new(amount: 100,
+                  user_id: 5,
+                  weeks: 2,
+                  base_currency: 'EUR',
+                  target_currency: 'RUB')
+
+  setup do
+    @result = ::Fixer::Service.new(RATE).calculate
+  end
+
+  test 'calculation result not nil' do
+    assert_not_equal(nil, @result)
+  end
+
+  test 'first date in array is today date' do
+    date = Time.now.utc.to_date
+    date_str = date.strftime('%Y-%m-%d')
+    assert_equal(date_str, @result.response[0]['date'])
+  end
+
+  test 'result length test' do
+    assert_equal(2, @result.response.length)
+  end
+end
